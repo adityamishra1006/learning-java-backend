9cb34969a9c699c745f9e374acf83a5fa2472900
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-10 11:34:47 +0530

    Add properties to Keyboard class

--- L-01/src/org/Aditya/Keyboard.java
+++ L-01/src/org/Aditya/Keyboard.java
@@ -1,4 +1,10 @@
 package org.Aditya;
 
 public class Keyboard {
+    String brand;
+    String type;         
+    int numberOfKeys;
+    boolean isWireless;
+    String layout;    
+    double price;
 }
