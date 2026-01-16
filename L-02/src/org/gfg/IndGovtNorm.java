c44213d9769c0ba0abc70320025c9b920ddb08ee
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2026-01-16 23:24:31 +0530

    Entity Creation

--- L-02/src/org/gfg/IndGovtNorm.java
+++ L-02/src/org/gfg/IndGovtNorm.java
@@ -5,7 +5,6 @@ public interface IndGovtNorm {
     public void indiaSpecificFunction();
 
     void method1();
-
     default String getDeviceLocation(){
         return "Not Supported in IndGovtNorm";
     }
